ca2b2dc0711f4b318ca73d6a1c5fc71b7eca08f8
pujaanil <pujaanil007@gmail.com>
2024-01-05 12:18:44 +0400

    Componet

---
+++ src/app/auth/login/login-component/login-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.scss']
+})
+export class LoginComponentComponent {
+
+}
